c2374656d234ff58d0c58763e3b66dc3f60d2386
feran <therealyoung247@gmail.com>
2024-10-23 13:44:09 +0100

    updated

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import Nav from './Nav'
+import Hero from './Hero'
+import About from './About'
+export default function App() {
+  return (
+    <div>
+     <Nav />
+     <Hero />
+     <About />
+    </div>
+  )
+}
