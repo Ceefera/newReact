20ca0c13d61d7b35ff8386303f573090f783f743
feran <therealyoung247@gmail.com>
2024-10-29 13:16:35 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import './App.css'
 import Nav from './Nav'
 import Hero from './Hero'
 import About from './About'
+import Services from './Services'
 export default function App() {
   return (
     <div>
      <Nav />
      <Hero />
      <About />
+     <Services />
     </div>
   )
 }
